108dc0cf7e164d7bd4b76b1d383666f39f21c18d
Sohang Chopra <sohangchopra@gmail.com>
2022-04-11 13:01:46 +0530

    Added Powershell Profile
    1. Change default file encoding to UTF 8. Not required since Powershell 6.
    2. Function Search-PyDocs - Search in Python Documentation

---
+++ profile.ps1
@@ -0,0 +1,25 @@
+# My Powershell Profile
+
+# NOTE: This is not required in Powershell 6+
+# Default file encoding is UTF 16 - change it to UTF 8
+# Without this, many CLI tools won't work, since they expect UTF 8 encoding.
+# Example:- (this code works with this line, but produces error otherwise).
+#      'print("Hello World!")' > temp.py
+#      python temp.py     # Doesn't work, because it can't decode UTF 16
+$PSDefaultParameterValues['*:Encoding'] = 'utf8'
+
+########## MISC FUNCTIONS #############
+
+function Search-PyDocs {
+	<#
+	.SYNOPSIS
+	Search for a query in Python 3 Documentation, and open results in Web Browser
+	#>
+	param(
+		[Parameter(mandatory=$true)]
+		[String] $SearchTerm
+	)
+	Start-Process https://docs.python.org/3/search.html?q=$SearchTerm
+}
+
+
